169b35eef86351cd75d95e6f7dfa024f15242d63
JKV21 <vtechgen21@gmail.com>
2023-07-02 14:18:08 +0200

    first commit

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import Feed from "./Feed";
+import Form from "./Form";
+import Profile from "./Profile";
+import Nav from "./Nav";
+import PromptCard from "./PromptCard";
+import Provider from "./Provider";
+
+export { Feed, Form, Profile, Nav, PromptCard, Provider };
